c464face5c9032ec7c61ee997d093497fd64bbbd
Diego Fernandes <diego.schell.f@gmail.com>
2025-08-11 20:51:33 -0300

    Aula 01

---
+++ server.ts
@@ -0,0 +1,64 @@
+import fastify from 'fastify'
+import crypto from 'node:crypto'
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+})
+
+const courses = [
+  { id: '1', title: 'Curso de Node.js' },
+  { id: '2', title: 'Curso de React' },
+  { id: '3', title: 'Curso de React Native' },
+]
+
+server.get('/courses', (request, reply) => {
+  return reply.send({ courses })
+})
+
+server.get('/courses/:id', (request, reply) => {
+  type Params = {
+    id: string
+  }
+
+  const params = request.params as Params
+  const courseId = params.id
+
+  const course = courses.find(course => course.id === courseId)
+
+  if (course) {
+    return { course }
+  }
+
+  return reply.status(404).send()
+})
+ 
+server.post('/courses', (request, reply) => {
+  type Body = {
+    title: string
+  }
+
+  const courseId = crypto.randomUUID()
+
+  const body = request.body as Body
+  const courseTitle = body.title
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: 'Título obrigatório.' })
+  }
+
+  courses.push({ id: courseId, title: courseTitle })
+
+  return reply.status(201).send({ courseId })
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
